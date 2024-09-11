a8333864536e5e1bbcb358f3fff70d49c41ca7b8
quochuybb <nqhsktt1@gmail.com>
2024-09-11 15:58:15 +0700

    first commit

---
+++ Todolist/src/ui/AuthenticationScreen/SignUpScreen.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Todolist.src.ui.AuthenticationScreen
+{
+    public partial class SignUpScreen : UserControl
+    {
+        public SignUpScreen()
+        {
+            InitializeComponent();
+        }
+
+        private void SignIn_Label_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void SignIn_Email_TextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
