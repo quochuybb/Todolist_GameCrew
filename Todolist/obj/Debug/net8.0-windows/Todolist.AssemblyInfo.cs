0b5c65262dbb3bb6eb4216a10e78396ba5a41557
DatAKAjezz <jezzthedead@gmail.com>
2024-09-17 20:54:52 +0700

    updatelast

--- Todolist/obj/Debug/net8.0-windows/Todolist.AssemblyInfo.cs
+++ Todolist/obj/Debug/net8.0-windows/Todolist.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Todolist")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b3b182f348c408b39464be1d27c72dddc0e8e012")]
 [assembly: System.Reflection.AssemblyProductAttribute("Todolist")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Todolist")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
